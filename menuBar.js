256c220f8bf18e3aac3f06fb0548891ffb7b4323
Glitch (hello-webpage) <none>
2020-01-11 18:39:43 +0000

    😅🏫 Checkpoint ./menuBar.js:6598775/136 ./index.html:6598775/415 ./style.css:6598775/68

--- menuBar.js
+++ menuBar.js
@@ -1,8 +1,3 @@
-function myFunction(x) {
-  x.classList.toggle("change")
-  x.classlist.toggle("container1");
-}
-
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function menu() {
@@ -11,7 +6,7 @@ function menu() {
 
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
-  if (!event.target.matches('.container')) {
+  if (!event.target.matches('.button')) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
