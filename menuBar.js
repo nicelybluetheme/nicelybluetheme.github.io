f09926c559367a8648d5cbb25d41be2c332417bb
Glitch (hello-webpage) <none>
2020-01-11 18:27:45 +0000

    🕌💠 Checkpoint ./style.css:6598775/38 ./about.html:6598775/278 ./menuBar.js:6598775/655 ./index.html:6598775/36

--- menuBar.js
+++ menuBar.js
@@ -2,3 +2,23 @@ function myFunction(x) {
   x.classList.toggle("change")
   x.classlist.toggle("container1");
 }
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function menu() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.container')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
