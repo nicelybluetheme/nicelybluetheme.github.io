f7c03594dc46fdc4c6a28cd4e05300dc5a5d6489
Glitch (hello-webpage) <none>
2020-01-11 19:14:42 +0000

    🎠👬 Checkpoint ./index.html:6598775/43 ./style.css:6598775/14 ./menuBar.js:6598775/14

--- menuBar.js
+++ menuBar.js
@@ -1,6 +1,6 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
-function menu() {
+function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
